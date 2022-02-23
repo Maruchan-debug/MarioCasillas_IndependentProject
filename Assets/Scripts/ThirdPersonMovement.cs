2d243b070fb269365adbae6a10e02e53913e3a50
Maruchan-debug <78717496+Maruchan-debug@users.noreply.github.com>
2022-02-23 03:23:09 -0800

    SharkRunner_GreyBox_Prototype1
    Simple greybox level of an island surrounded by water
    with more or less decent 3rd person movement and camera angles. Sharks are up next.

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            //Atan 2 is a Mathematical Function that returns the Angle between the X Axis
+            //And the Angle starting at Zero and ending at x,y
+
+            //Atan function gives us our angle in radians, so we utilize
+            //"Rad2Deg" to convert the angle from Radians to Degrees
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            //Quaternion.Euler allows us to input for x,y and z
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            //We turn our Quaternion into a direction by multiplying by our vector
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
